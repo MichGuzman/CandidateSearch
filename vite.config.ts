dd5c870a22187f15d58e678f58df551c64b020ac
MichGuzman <michguzman@live.com>
2025-04-08 19:58:06 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['candidate-search-project.onrender.com'], // tu dominio en Render
+  },
 });
